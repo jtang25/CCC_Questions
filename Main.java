5dd0f56425dd64cf23fe0f43c69ebd9e61878436
Jason Tang <jasontang0226@gmail.com>
2022-11-21 00:13:59 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        a=a*8;
+        b=b*3;
+        int left = 28-a-b;
+        System.out.println(Math.abs(left));
+    }
+}
